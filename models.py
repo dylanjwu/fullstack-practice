893c053763a0088ed973d2e8789b7be560a4f2a7
[dylanjwu] <dylanjwu@gmail.com>
2020-09-29 15:17:52 -0700

    first commit, working code from tutorial

---
+++ models.py
@@ -0,0 +1,33 @@
+#schema, create, run to restart database
+import sqlite3
+
+"""
+terminal:
+    sqlite3 songs.db
+    .table
+    .schema
+    .exit
+"""
+
+def drop_table():
+    with sqlite3.connect('songs.db') as connection:
+        c = connection.cursor()
+        c.execute("""DROP TABLE IF EXISTS songs;""")
+    return True
+
+def create_db():
+    with sqlite3.connect('songs.db') as connection:
+        c = connection.cursor()
+        table = """CREATE TABLE songs(
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            artist TEXT NOT NULL,
+            title TEXT NOT NULL,
+            rating INTEGER NOT NULL
+        );
+        """
+        c.execute(table)
+    return True
+
+if __name__ == '__main__':
+    drop_table()
+    create_db()
